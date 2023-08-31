3e27680f68802ad9421a6ad85621c3a02fa1a127
Riccardo <riccardo960@gmail.com>
2023-08-31 13:50:04 +0200

    terraform for lambda function

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda_zip_file" {
+  type        = "zip"
+  source_file = "${path.module}/dist/index.js"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "ip_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = data.archive_file.lambda_zip_file.output_path
+  function_name = "ip_recorder"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
+
+  runtime = "nodejs18.x"
+}
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "main"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_handler" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.ip_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "get_handler" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "GET /"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_handler.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ip_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
